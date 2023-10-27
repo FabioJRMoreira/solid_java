b9f7d30c1dc50bfa26a5438228f56d0600aad438
fabio <tulkas.fabio@hotmail.com>
2023-10-26 22:22:55 -0400

    Primeiro Commit

--- src/br/com/alura/rh/service/reajuste/ReajusteService.java
+++ src/br/com/alura/rh/service/reajuste/ReajusteService.java
@@ -12,7 +12,6 @@ public class ReajusteService {
 	public ReajusteService(List<ValidacaoReajuste> validacoes) {
 		this.validacoes = validacoes;
 	}
-
 	public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
 		this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
